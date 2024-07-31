8c0ffe82cfb58715c8c2aa071e432497e5626dc0
Anthony <anthony@behaviourlab.com>
2024-08-01 00:58:22 +0100

    feat(reward-logger): add reward logger file to main to reward episode rewward changes

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
 import optuna
-from samplers import SimulatedAnnealingSampler
-from utils.training import objective, define_objective
-from utils.evaluation import log_evaluation
+import logging
+
+from utils.evaluation_logger import log_evaluation
+from utils.training import objective, define_objective, LoggerRewardCallback
 from envs.satellite_env import SatelliteEnv
+from samplers import SimulatedAnnealingSampler
+
+log_file = 'rewards_log.txt'
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_file, mode='w'),  # overwrite the log file each run
+        logging.StreamHandler()  # optionally log to console as well
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     sampler = SimulatedAnnealingSampler()
     study = optuna.create_study(storage="sqlite:///rl.db", direction="maximize", sampler=sampler)
-    study.optimize(objective, n_trials=600)
+    study.optimize(objective, n_trials=2)
 
     print(f"Best trial: {study.best_trial.params}")
     print(f"Best reward: {study.best_trial.value}")
@@ -15,7 +30,8 @@ if __name__ == "__main__":
     model = define_objective(study.best_trial)
 
     env = SatelliteEnv()
-    model.learn(total_timesteps=25000)
+    logger_reward_callback = LoggerRewardCallback(logger)
+    model.learn(total_timesteps=200*100,callback=logger_reward_callback)
     model.save("ppo_satellite")
 
-    log_evaluation(model, env)
+    log_evaluation(model, env, n_eval_episodes=50, total_timestep=1000)
